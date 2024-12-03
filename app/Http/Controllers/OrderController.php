384080ac35c9c0efa4e02d50d8cccc30b2fbf17c
OmarJamalKahil <omar.kahil.2004.9@gmail.com>
2024-12-03 20:18:45 +0300

    .........

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -16,30 +16,9 @@ class OrderController extends Controller
     **/
     public function getPurchasedOrders(Request $request)
     {
-<<<<<<< HEAD
+
         $orders = Order::where('user_id', $request->user_id)->where('status_id', 1)->select('id', 'total', 'shipping_cost')->get();
         return response()->json($orders, 200);
-=======
-        $orders = Order::where('user_id', $request->id)->get();
-        $purchasedOrders = [];
-        $productsPerOrder = [];
-        foreach ($orders as $order) {
-            $status = Status::where('id', $order->status_id)->get();
-            if ($status[0]['name'] == 'Purchased') {
-                $productsPerOrder[] = OrderController::getOrderProducts($order->id);
-
-                $purchasedOrders[] = $order;
-            }
-        }
-        foreach ($purchasedOrders as $key => $order) {
-            $order->products =  $productsPerOrder[$key]->select('name', 'price', 'shop_id');
-            // $s = new shopController();
-            // foreach ($order->products as $product) {
-            // $product['shop_id'] = $s->getShop($product['shop_id'])->name;
-            // }
-        }
-        return response()->json($purchasedOrders, 200);
->>>>>>> ac99f303cc73646adbc7bbe257831b29ed11b4bc
     }
 
     /** 
@@ -47,7 +26,6 @@ class OrderController extends Controller
     **/
     public function getOrderProducts($id)
     {
-<<<<<<< HEAD
         $products = Order::findOrFail($id)->products->map(function ($product) {
             return [
                 'name' => $product->name,
@@ -57,10 +35,6 @@ class OrderController extends Controller
             ];
         });
         return response()->json($products, 200);
-=======
-        $products = Order::findOrFail($id)->products;
-        return $products;
->>>>>>> ac99f303cc73646adbc7bbe257831b29ed11b4bc
     }
 
     /** 
@@ -70,16 +44,10 @@ class OrderController extends Controller
     {
         $order = Order::findOrFail($id);
         $total = 0;
-<<<<<<< HEAD
         $file = $this->getOrderProducts($order->id);
         $products = json_decode($file->getContent(), true);
         foreach ($products as $product) {
             $total += $product['price'] * $product['quantity'];
-=======
-        $products = OrderController::getOrderProducts($order->id);
-        foreach ($products as $product) {
-            $total += $product->price * $product->pivot->quantity;
->>>>>>> ac99f303cc73646adbc7bbe257831b29ed11b4bc
         }
         $order->update(['total' => $total]);
     }
